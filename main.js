b17269304e06c5b37d7dec2a04694652cf910a59
Jacob <jakeboone@aol.com>
2014-06-25 12:27:15 -0600

    Victims Complete

---
+++ main.js
@@ -0,0 +1,16 @@
+// VICTIMS
+
+var victimsMessage = prompt("How many victims do you wish to enter?")
+		var nameArray = []
+		var phoneArray = []
+		var streetArray = []
+
+	
+	for (var i = 0; i < victimsMessage; i++) {
+			nameArray.push(prompt("Enter Name"))
+			phoneArray.push(prompt("Enter Phone Number"))
+			streetArray.push(prompt("Enter Street Address"))
+}
+
+		
+
